11dd923b99ae6c4f4390753840313b7d62a833c4
JoZonneveld <joostz14@gmail.com>
2017-02-08 15:50:45 +0100

    .

---
+++ Periode 3/Homework1/Homework1/Excercise1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Excercise1
+    { 
+        static void Main(string[] args)
+        {
+            string result = "";
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    result = result + "*";
+                }
+                result = result + "\n";                
+            }
+            Console.Write(result);
+            Console.ReadLine();
+        }
+    }
+}
