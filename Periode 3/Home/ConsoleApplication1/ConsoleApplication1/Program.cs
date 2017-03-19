49820d5a8ef3ee9fb0c25191987fcb06380978c8
JoZonneveld <joostz14@gmail.com>
2017-03-19 22:21:14 +0100

    .

---
+++ Periode 3/Home/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void print(string output)
+        {
+            Console.WriteLine(output);
+        }
+
+        static void end()
+        {
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            print("Voer een datum in: Voorbeeld(23-04-1996)");
+            string input = Console.ReadLine();
+            print(input);
+
+            Char split = '-';
+            String[] substrings = input.Split(split);
+            print("Dag: " + substrings[0]);
+            print("Maand: " + substrings[1]);
+            print("Jaar: " + substrings[2]);
+
+            end();
+        }
+    }
+}
